84dbb0590f1f91a020e5272a59ebdb1c166f89a3
Paulo Morais <paulo@bry.com.br>
2022-06-01 11:19:26 -0300

    Realizadas novas implementações

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,6 +22,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -41,8 +43,9 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	//	@JsonIgnore
-	@ManyToOne // Muitos restaurantes tem uma cozinha
+	// @JsonIgnore
+	// @JsonIgnoreProperties("hibernateLazyInitializer")
+	@ManyToOne // (fetch = FetchType.LAZY) // Muitos restaurantes tem uma cozinha
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
@@ -64,7 +67,7 @@ public class Restaurante {
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;	
 	
-	@JsonIgnore
+	// @JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,14 @@ import com.algaworks.algafood.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, 
 												JpaSpecificationExecutor<Restaurante> {
-
+	
+	// Errata: se um restaurante não tiver nenhuma forma de pagamento associada a ele,
+	// esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
+	// Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
+	//	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	@Query("select distinct r from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
+	
 	// Between configura para pesquisa taxa entre os valores de taxa inicial e taxa final
 	// documentação: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
