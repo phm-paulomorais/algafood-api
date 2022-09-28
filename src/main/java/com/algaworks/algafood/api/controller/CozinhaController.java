3237d4a3a8e9f707e25dbce91c3ba4386d9f1eba
Paulo Morais <paulo@bry.com.br>
2022-09-28 11:41:34 -0300

    Implementacao da aula 13.8

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -44,10 +48,16 @@ public class CozinhaController {
 	
 	// @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@GetMapping
-	public List<CozinhaModel> listar() {
-	    List<Cozinha> todasCozinhas = cozinhaRepository.findAll();
+	public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
+		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 	    
-	    return cozinhaModelAssembler.toCollectionModel(todasCozinhas);
+		List<CozinhaModel> cozinhasModel = cozinhaModelAssembler
+				.toCollectionModel(cozinhasPage.getContent());
+		
+		Page<CozinhaModel> cozinhasModelPage = new PageImpl<>(cozinhasModel, pageable, 
+				cozinhasPage.getTotalElements());
+		
+		return cozinhasModelPage;
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
