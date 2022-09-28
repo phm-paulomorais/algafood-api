35ea989d756aefd14068d46e3d952535fca4ceee
Paulo Morais <paulo@bry.com.br>
2022-09-28 12:05:32 -0300

    Implementação da aula 13.9

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -6,6 +6,10 @@ import javax.validation.Valid;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -76,10 +80,17 @@ public class PedidoController {
 
     
     @GetMapping
-    public List<PedidoResumoModel> listar(PedidoFilter filtro) {
-    	List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+    public Page<PedidoResumoModel> listar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable) {
+    	Page<Pedido> pedidosPage = pedidoRepository.findAll(
+    	           PedidoSpecs.usandoFiltro(filtro), pageable);
+    	 
+    	List<PedidoResumoModel> pedidosResumoModel = pedidoResumoModelAssembler
+    	           .toCollectionModel(pedidosPage.getContent());
+    	 
+    	Page<PedidoResumoModel> pedidosResumoModelPage = new PageImpl<>(
+    	           pedidosResumoModel, pageable, pedidosPage.getTotalElements());
         
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+    	return pedidosResumoModelPage;
     }
     
     
