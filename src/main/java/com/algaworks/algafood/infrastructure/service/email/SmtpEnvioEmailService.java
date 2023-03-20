24e9953f9ba788b1685cd7e13e31bef0efd4bbb2
Paulo <phm.paulomorais@gmail.com>
2023-03-20 09:06:02 -0300

    Iniciando configuração no ambiente de producao

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
@@ -24,7 +20,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	private EmailProperties emailProperties;
 	
 	@Autowired
-	private Configuration freemarkerConfig;
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -39,7 +35,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	}
 	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-	    String corpo = processarTemplate(mensagem);
+	    String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 	    
 	    MimeMessage mimeMessage = mailSender.createMimeMessage();
 	    
@@ -52,16 +48,5 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	    return mimeMessage;
 	}
 	
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-			Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
-			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(
-					template, mensagem.getVariaveis());
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		}
-	}
-
 
 }
